c3a843e346d13edff75e389e240f9f4f693b1393
Rudio <rudio.fida_cyrille@etu.sorbonne-universite.fr>
2023-11-10 11:00:48 +0100

    Creating project structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SentimentStay
 Hotel review sentiment analysis model and deployment
+
+Link to the kaggle [dataset](https://www.kaggle.com/datasets/andrewmvd/trip-advisor-hotel-reviews) 
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Hotel reviews sentiment analysis model ddeveloppement and deployment',
+    author='Rudio',
+    license='MIT',
+)
